48b025d34c451345ebe1270700a885c7f26308f7
Travis Hoki <travishoki@Hoki-CB-Mac.hsd1.ut.comcast.net>
2020-11-05 00:12:46 -0700

    search using state

--- projects/weather-app/src/app/weather-form/weather-form.component.ts
+++ projects/weather-app/src/app/weather-form/weather-form.component.ts
@@ -22,12 +22,66 @@ export class WeatherFormComponent implements OnInit {
 
 	form = new FormGroup({
 		city: new FormControl('', Validators.required),
+		state: new FormControl('', Validators.required),
 	});
 
+	statesList = [
+		'Alabama',
+		'Alaska',
+		'Arizona',
+		'Arkansas',
+		'California',
+		'Colorado',
+		'Connecticut',
+		'Delaware',
+		'Florida',
+		'Georgia',
+		'Hawaii',
+		'Idaho',
+		'Illinois',
+		'Indiana',
+		'Iowa',
+		'Kansas',
+		'Kentucky',
+		'Louisiana',
+		'Maine',
+		'Maryland',
+		'Massachusetts',
+		'Michigan',
+		'Minnesota',
+		'Mississippi',
+		'Missouri',
+		'Montana',
+		'Nebraska',
+		'Nevada',
+		'New Hampshire',
+		'New Jersey',
+		'New Mexico',
+		'New York',
+		'North Carolina',
+		'North Dakota',
+		'Ohio',
+		'Oklahoma',
+		'Oregon',
+		'Pennsylvania',
+		'Rhode Island',
+		'South Carolina',
+		'South Dakota',
+		'Tennessee',
+		'Texas',
+		'Utah',
+		'Vermont',
+		'Virginia',
+		'Washington',
+		'West Virginia',
+		'Wisconsin',
+		'Wyoming',
+	];
+
 	submit() {
-		const { city } = this.form.value;
+		const { city, state } = this.form.value;
 		const apiKey = 'c574c5835383440bcc0d8af84b4736cf';
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}`;
 
 		this.loading = true;
 
