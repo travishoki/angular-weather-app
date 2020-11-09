974662d332f2f542b1c2685680c5f36d4ee73eda
Travis Hoki <travishoki@Hoki-CB-Mac.hsd1.ut.comcast.net>
2020-11-09 01:34:14 -0700

    header unit tests

---
+++ projects/weather-app/src/app/header/header.component.test.ts
@@ -0,0 +1,28 @@
+// app.component.spec.ts
+import { TestBed, async } from '@angular/core/testing';
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				HeaderComponent
+			],
+		}).compileComponents();
+	}));
+
+	it('should create the component', () => {
+		const fixture = TestBed.createComponent(HeaderComponent);
+		const app = fixture.debugElement.componentInstance;
+
+		expect(app).toBeTruthy();
+	});
+
+	it('should render title in a h1 tag', () => {
+		const fixture = TestBed.createComponent(HeaderComponent);
+		fixture.detectChanges();
+
+		const compiled = fixture.debugElement.nativeElement;
+		expect(compiled.querySelector('h1').textContent).toContain('Weather App');
+	});
+});
